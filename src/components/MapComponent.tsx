f9d5f81959f30d5d4756adf1761f2c821573c228
Christopher Rauser <chris.rauser@hotmail.de>
2024-12-15 21:57:33 +0100

    map shows Stuttgart Viertel

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import geojsonData from '../data/Stgt_Stadtteile.geojson';
+import geojsonData from '../data/Stgt_Stadtteile.json';
+import { GeoJsonObject } from 'geojson';
 
 const MapComponent: React.FC = () => {
   // const position = [51.505, -0.09]
+  console.log('GeoJSON Data:', geojsonData); // Debugging line
+
 
   const onEachFeature = (feature: any, layer: any) => {
     const { STADTTEILNAME } = feature.properties;
-    layer.bindPopup(`<strong>${STADTTEILNAME}</strong>`);
+    const { STADTBEZIRKNAME } = feature.properties;
+    const popupContent = `<strong>${STADTTEILNAME}</strong><br>Stadtbezirk: ${STADTBEZIRKNAME}`;
+  
+    layer.on('mouseover', function () {
+      layer.bindPopup(popupContent).openPopup();
+    });
+  
+    layer.on('mouseout', function () {
+      layer.closePopup();
+    });
   };
 
   return (
     <MapContainer center={[48.7758, 9.1829]} zoom={12} style={{ height: '500px', width: '100%' }}>
-      <TileLayer
-        attribution="&copy; OpenStreetMap contributors"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <GeoJSON data={geojsonData} onEachFeature={onEachFeature} />
+      <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png" />
+       <GeoJSON data={geojsonData as GeoJsonObject} onEachFeature={onEachFeature}/> 
   </MapContainer>
   );
 };
