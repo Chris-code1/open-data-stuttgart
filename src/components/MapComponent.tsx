eeb6d79b483f6e0642b7c2ca071b5cd6fc153249
Christopher Rauser <chris.rauser@hotmail.de>
2024-12-17 20:16:20 +0100

    styling map

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -9,14 +9,14 @@ import { useEffect } from 'react';
 const MapComponent: React.FC = () => {
   // Color scale function
   const getColor = (d: number) => {
-    return d > 50 ? '#800026' :
-            d > 40 ? '#BD0026' :
-            d > 30 ? '#E31A1C' :
-            d > 20 ? '#FC4E2A' :
-            d > 10 ? '#FD8D3C' :
-            d > 5  ? '#FEB24C' :
-            d > 0  ? '#FED976' :
-                    '#FFEDA0';
+    return d > 30 ? '#0000FF' :  // Blue
+           d > 25 ? '#0033FF' :  // Lighter Blue
+           d > 20 ? '#0066FF' :  // Even Lighter Blue
+           d > 15 ? '#0099FF' :  // Light Blue
+           d > 10 ? '#00CCFF' :  // Lighter Blue
+           d > 5  ? '#00FFFF' :  // Very Light Blue
+           d > 0 ? '#CCFFFF' :  // Very Light Pale Blue
+             d == 0 ? '#FFFFFF' : '#FFFFFF';   // White
   };
 
   const style = (feature: any) => {
@@ -56,25 +56,38 @@ const MapComponent: React.FC = () => {
 
     useEffect(() => {
       const legend = new L.Control({ position: 'bottomright' });
+      const title = new L.Control({ position: 'topright' });
 
       legend.onAdd = () => {
         const div = L.DomUtil.create('div', 'info legend');
-        const grades = [0, 5, 10, 20, 30, 40, 50];
-        const labels = [];
-
+        const grades = [0, 5, 10, 15, 20, 25, 30];
+      
+        div.style.color = 'white'; // Set font color to white
+      
         for (let i = 0; i < grades.length; i++) {
           div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            '<i style="background:' + getColor(grades[i] + 1) + '; width: 18px; height: 18px; display: inline-block; margin-right: 8px;"></i> ' +
             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
         }
-
+      
         return div;
       };
 
+      title.onAdd = () => {
+        const div = L.DomUtil.create('div', 'map-title');
+        div.innerHTML = '<h2>Sozialwohnungen in %</h2>';
+        div.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
+        div.style.padding = '10px';
+        div.style.borderRadius = '5px';
+        return div;
+      };
+      
+      title.addTo(map);
       legend.addTo(map);
 
       return () => {
         legend.remove();
+        title.remove();
       };
     }, [map]);
 
@@ -82,11 +95,10 @@ const MapComponent: React.FC = () => {
   };
 
   return (
-    <MapContainer center={[48.7758, 9.1829]} zoom={11} style={{ height: '500px', width: '50%' }}>
+    <MapContainer center={[48.7758, 9.1829]} zoom={11} style={{ height: '500px', width: '50%' }} scrollWheelZoom={false} dragging={false}>
       <Legend />
-      {/* <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png" /> */}
-       <GeoJSON data={geojsonData as GeoJsonObject} style={style} onEachFeature={onEachFeature}/> 
-       
+      <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png" />
+      <GeoJSON data={geojsonData as GeoJsonObject} style={style} onEachFeature={onEachFeature}/> 
   </MapContainer>
   );
 };
